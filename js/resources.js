88c70eb770c3dede9a718b9faa5820f2c06682aa
Maxwell Karp <maxwell.karp@yahoo.com>
2015-02-11 16:11:04 -0800

    hacking at friend creep

--- js/resources.js
+++ js/resources.js
@@ -12,8 +12,10 @@ game.resources = [
 	{name: "player", type:"image", src: "data/img/orcSpear.png"},
 	//loads the tower entity from the data folder, allowing it to display it on the map
 	{name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
-	//loads the creep entity from the data folder, allowing it to display it on the map
+	//loads the brainmonster entity from the data folder, allowing it to display it on the map
 	{name: "creep1", type:"image", src: "data/img/brainmonster.png"},
+	//loads the oneeye entity from the data folder, allowing it to display it on the map
+	{name: "creep2", type:"image", src: "data/img/elf.png"},
 
 	/* Atlases 
 	 * @example
