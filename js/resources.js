c8e8cbb86a2c853f2383a62ba41ab1e7a444ca1a
Maxwell Karp <maxwell.karp@yahoo.com>
2015-02-20 10:52:50 -0800

    video 26 coded and commented

--- js/resources.js
+++ js/resources.js
@@ -16,7 +16,8 @@ game.resources = [
 	{name: "creep1", type:"image", src: "data/img/brainmonster.png"},
 	//loads the oneeye entity from the data folder, allowing it to display it on the map
 	{name: "creep2", type:"image", src: "data/img/gloop.png"},
-
+	//loads the title-screen image from the data folder, allowing it to display it on the map
+	{name: "title-screen", type:"image", src: "data/img/title.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
