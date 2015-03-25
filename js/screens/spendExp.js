d2d5974f7c5fd8a3eb9f7f1e99acc37bef75468e
Maxwell Karp <maxwell.karp@yahoo.com>
2015-03-25 11:48:30 -0700

    a little bit of work on spendExp.js

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -13,7 +13,10 @@ game.SpendExp = me.ScreenObject.extend({
 		me.input.bindKey(me.input.KEY.F4, "F4");
 		me.input.bindKey(me.input.KEY.F5, "F5");
 		//builds variable to store the cost of exp1
-		var exp1cost = ((game.data.exp4 + 1) * 10);
+		var exp1cost = ((game.data.exp1 + 1) * 10);
+		var exp2cost = ((game.data.exp2 + 1) * 10);
+		var exp3cost = ((game.data.exp3 + 1) * 10);
+		var exp4cost = ((game.data.exp4 + 1) * 10);
 
 		//for new game
 		me.game.world.addChild(new (me.Renderable.extend({
@@ -21,7 +24,7 @@ game.SpendExp = me.ScreenObject.extend({
 				//calls super class and positions it
 				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
 				//sets the font to arial, the size 46, and colors it white
-				this.font = new me.Font("halfelven", 36, "gold");
+				this.font = new me.Font("halfelven", 33, "gold");
 			},
 			//function that sets up the writing
 			draw: function(renderer){
@@ -32,11 +35,11 @@ game.SpendExp = me.ScreenObject.extend({
 				//inserts the message "Current EXP: " and adds the current exp, the cost, and sets where writing starts
 				this.font.draw(renderer.getContext(), "F1: INCREASE GOLD PRODUCTION. CURRENT LEVEL: " + game.data.exp.toString() + ". COST: " + exp1cost, this.pos.x, this.pos.y + 100);
 				//inserts the message "Current EXP: " and adds the current exp, the cost, and sets where writing starts
-				this.font.draw(renderer.getContext(), "F2: INCREASE SPEED. CURRENT LEVEL: " + game.data.exp2.toString() + ". COST: " + ((game.data.exp2 + 1) * 10), this.pos.x, this.pos.y + 150);
+				this.font.draw(renderer.getContext(), "F2: INCREASE SPEED. CURRENT LEVEL: " + game.data.exp2.toString() + ". COST: " + exp2cost, this.pos.x, this.pos.y + 150);
 				//inserts the message "Current EXP: " and adds the current exp, the cost, and sets where writing starts
-				this.font.draw(renderer.getContext(), "F3: INCREASE DAMAGE. CURRENT LEVEL: " + game.data.exp3.toString() + ". COST: " + ((game.data.exp3 + 1) * 10), this.pos.x, this.pos.y + 200);
+				this.font.draw(renderer.getContext(), "F3: INCREASE DAMAGE. CURRENT LEVEL: " + game.data.exp3.toString() + ". COST: " + exp3cost, this.pos.x, this.pos.y + 200);
 				//inserts the message "F4: INCREASE STARTING HEALTH. CURRENT LEVEL: " and adds the current exp, the cost, and sets where writing starts
-				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH. CURRENT LEVEL: " + game.data.exp4.toString() + ". COST: " + ((game.data.exp4 + 1) * 10), this.pos.x, this.pos.y + 250);
+				this.font.draw(renderer.getContext(), "F4: INCREASE STARTING HEALTH. CURRENT LEVEL: " + game.data.exp4.toString() + ". COST: " + exp4cost, this.pos.x, this.pos.y + 250);
 			}
 			
 		})));
@@ -59,11 +62,50 @@ game.SpendExp = me.ScreenObject.extend({
 				}
 			}
 			//runs if F2 button is pressed
-			if (action === "F2") {}
+			if (action === "F2") {
+				//runs if player has at least 10 exp
+				if (game.data.exp >= 10) {
+					//adds 1 to exp1
+					game.data.exp2 += 1;
+					//subtracts the cost of exp1 from exp
+					game.data.exp -= exp2cost;
+					//goes to play screen
+					me.state.change(me.state.PLAY);
+				}
+				else{
+					console.log("Not enough exp");
+				}
+			}
 			//runs if F3 button is pressed
-			if (action === "F3") {}
+			if (action === "F3") {
+				//runs if player has at least 10 exp
+				if (game.data.exp >= 10) {
+					//adds 1 to exp1
+					game.data.exp3 += 1;
+					//subtracts the cost of exp1 from exp
+					game.data.exp -= exp3cost;
+					//goes to play screen
+					me.state.change(me.state.PLAY);
+				}
+				else{
+					console.log("Not enough exp");
+				}
+			}
 			//runs if F4 button is pressed
-			if (action === "F4") {}
+			if (action === "F4") {
+				//runs if player has at least 10 exp
+				if (game.data.exp >= 10) {
+					//adds 1 to exp1
+					game.data.exp4 += 1;
+					//subtracts the cost of exp1 from exp
+					game.data.exp -= exp4cost;
+					//goes to play screen
+					me.state.change(me.state.PLAY);
+				}
+				else{
+					console.log("Not enough exp");
+				}
+			}
 			//runs if F5 button is pressed
 			if (action === "F5") {
 				//runs the play screen
